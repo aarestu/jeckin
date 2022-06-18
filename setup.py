eeb7d6a6bd51abc12d914490d872f097b5dafca5
restu suhendar <restu.suhendar@gmail.com>
2022-06-19 00:35:17 +0700

    upgrade description package

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
+import pathlib
+
 from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='jeckin',
-    version='0.1.0',
+    version='0.0.1-beta',
     python_requires=">=3.7, <4",
+    description="http injector",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(include=['app', 'jeckin.*']),
     install_requires=[
     ],
 
     entry_points={
         'console_scripts': ['jeckin=jeckin.main:main']
-    }
+    },
+
+    keywords=['python', 'http', 'http injector'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
