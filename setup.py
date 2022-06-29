4394f783eab020dcc76be50b940ee80d90614829
restu suhendar <restu.suhendar@gmail.com>
2022-06-29 22:29:23 +0700

    fix direct -> ssl/tls -> ssh handler remove loging from ssh

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='jeckin',
-    version='0.0.1b6',
+    version='0.0.1b7',
     python_requires=">=3.7, <4",
 
     author="Restu Suhendar",
