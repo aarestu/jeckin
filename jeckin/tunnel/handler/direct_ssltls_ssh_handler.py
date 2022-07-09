3469250c81f7befabb0dfaf27ffcf63f885cd056
restu suhendar <restu.suhendar@gmail.com>
2022-07-10 01:35:39 +0700

    support ssl client_auth and server_auth (#5)
    * support setting net interface
    
    * fix error BINDTODEVICE need to be byte object like
    
    * ssh native direct to jeckin socket and disable Corkscrew
    
    * support ssl client_auth and server_auth

--- jeckin/tunnel/handler/direct_ssltls_ssh_handler.py
+++ jeckin/tunnel/handler/direct_ssltls_ssh_handler.py
@@ -12,17 +12,20 @@ def get_direct_ssl_tls_ssh_handler(config, ssh_account):
             self.target_host = ssh_account.get("host")
             self.target_port = int(ssh_account.get("port"))
             self.payload = config.get("payload", self.payload).encode()
+
             self.sni = config.get("sni")
+            self.ssl_auth = config.get("ssl_auth")
             self.protocol = config.get("protocol")
             if not self.sni:
                 raise ValueError("sni required")
+
             self.net_interface = config.get("net_interface")
             super(Handler, self).setup()
 
         def create_sock_proxy(self):
             self.log(INFO, f'Connecting to remote {self.target_host} port {self.target_port}')
 
-            self.sock_proxy_http = self._get_sock(self.target_host, self.target_port)
+            self.sock_proxy_http = self._get_sock(self.sni, self.target_port)
             self.sock_proxy = self.warp_sock_to_ssl_tls(self.sock_proxy_http)
             return self.sock_proxy
 
